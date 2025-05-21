bcff2b1dfd162911a2da51b3cc35daa7c1f0720d
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-05-21 09:25:45 -0500

    added recepicollection

--- index.js
+++ index.js
@@ -31,6 +31,30 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+         const recipeCollection = client.db('recipeDB').collection('recipes');
+        
+        app.get('/recipes', async (req, res) => {
+           
+            const result = await recipeCollection.find().toArray();
+            res.send(result);
+
+        })
+
+
+
+
+        app.post('/recipes', async (req, res) => {
+            const newRecipe = req.body;
+           console.log(newRecipe);
+             const result = await recipeCollection.insertOne(newRecipe);
+             res.send(result);
+        })
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,7 +64,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 
 
